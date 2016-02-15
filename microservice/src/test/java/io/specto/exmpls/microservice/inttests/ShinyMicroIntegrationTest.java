1583ca81103f2f04f70c6b7bbc8e1e1c2abb6c32
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-02-15 15:04:53 +0000

    Add basic integration test for microservice

---
+++ microservice/src/test/java/io/specto/exmpls/microservice/inttests/ShinyMicroIntegrationTest.java
@@ -0,0 +1,30 @@
+package io.specto.exmpls.microservice.inttests;
+
+import io.specto.exmpls.microservice.Application;
+import io.specto.exmpls.microservice.model.ComplexObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = "TEST")
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest
+public class ShinyMicroIntegrationTest {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void test() {
+        ComplexObject[] complexObjects =
+                restTemplate.getForObject("http://localhost:8090/speedyRequest", ComplexObject[].class);
+        assertTrue(complexObjects.length == 1);
+    }
+
+}
