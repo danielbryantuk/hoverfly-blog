2975cc9f28271119f24dd10df8319a6bc75ac995
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-02-15 15:23:54 +0000

    Add basic hoverfly process runner to tests

--- microservice/src/test/java/io/specto/exmpls/microservice/inttests/ShinyMicroIntegrationTest.java
+++ microservice/src/test/java/io/specto/exmpls/microservice/inttests/ShinyMicroIntegrationTest.java
@@ -2,6 +2,8 @@ package io.specto.exmpls.microservice.inttests;
 
 import io.specto.exmpls.microservice.Application;
 import io.specto.exmpls.microservice.model.ComplexObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -18,7 +20,23 @@ import static org.junit.Assert.assertTrue;
 @WebIntegrationTest
 public class ShinyMicroIntegrationTest {
 
+    private static final String HOVERFLY_LOCATION = "/Users/danielbryant/Documents/dev/daniel-bryant-uk/hoverfly-blog/microservice/";
+    private static Process hoverflyProcess;
+
     private RestTemplate restTemplate = new RestTemplate();
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        ProcessBuilder builder = new ProcessBuilder()
+                .inheritIO()
+                .command(HOVERFLY_LOCATION + "hoverfly");
+        hoverflyProcess = builder.start();
+    }
+
+    @AfterClass
+    public static void teardown() throws Exception {
+        hoverflyProcess.destroy();
+    }
 
     @Test
     public void test() {
