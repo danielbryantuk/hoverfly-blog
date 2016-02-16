eb34efc26557e4f0a2b12566a40cd669acecbf97
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-02-16 13:26:18 +0000

    Add params to controllers

--- microservice/src/main/java/io/specto/exmpls/microservice/controllers/ShinyMicroController.java
+++ microservice/src/main/java/io/specto/exmpls/microservice/controllers/ShinyMicroController.java
@@ -5,6 +5,7 @@ import io.specto.exmpls.microservice.services.ComplexObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -17,8 +18,9 @@ public class ShinyMicroController {
 
     @RequestMapping("/speedyRequest")
     @ResponseBody
-    public List<ComplexObject> speedyRequest() {
-        return complexObjectService.getLatestComplexObjects();
+    public List<ComplexObject> speedyRequest(@RequestParam(value = "origin", required = false) final String origin,
+                                             @RequestParam(value = "destination", required = false) final String destination) {
+        return complexObjectService.getLatestComplexObjects(origin, destination);
     }
 
 }
--- microservice/src/main/java/io/specto/exmpls/microservice/services/ComplexObjectService.java
+++ microservice/src/main/java/io/specto/exmpls/microservice/services/ComplexObjectService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ComplexObjectService {
@@ -14,9 +16,14 @@ public class ComplexObjectService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public List<ComplexObject> getLatestComplexObjects() {
+    public List<ComplexObject> getLatestComplexObjects(final String origin, final String destination) {
+        Map<String, String> urlVariables = new HashMap<>();
+        urlVariables.put("origin", origin);
+        urlVariables.put("destination", destination);
+
         ComplexObject[] objects =
-                restTemplate.getForObject("http://localhost:8080/slowFragileRequest", ComplexObject[].class);
+                restTemplate.getForObject("http://localhost:8080/slowFragileRequest", ComplexObject[].class, urlVariables);
+
         // Process ComplexObjects into something else
         return Arrays.asList(objects);
     }
